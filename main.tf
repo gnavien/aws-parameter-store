267c6248ce367c9d7ef63b923be43c8e5b83b220
jayasathyag <jayasathyag@gmail.com>
2024-08-29 15:08:44 +0530

    update

--- main.tf
+++ main.tf
@@ -4,13 +4,14 @@ resource "aws_ssm_parameter" "params" {
   type  = var.params[count.index].type
   value = var.params[count.index].value
 }
-
+# we have parameters for each component like frontend, catalgue, etc
+# we need to go to roboshop-ansible --> frontend --> files --> roboshop.conf
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.navien.site", type = "string" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.navien.site", type = "string" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.navien.site", type = "string" },
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.navien.site", type = "string" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.navien.site", type = "string" },
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.navien.site:8080/", type = "string" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.navien.site:8080/", type = "string" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.navien.site:8080/", type = "string" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.navien.site:8080/", type = "string" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.navien.site:8080/", type = "string" },
      ]
 }
